af23689b9f9ef2dbb9609c5e097a7db7c65af22a
Raksha C C <rakshacc4@gmail.com>
2026-09-24 15:21:09 +0530

    Time: 2 ms (15.18%), Space: 10 MB (6.16%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.cpp
+++ 0020-valid-parentheses/0020-valid-parentheses.cpp
@@ -1,26 +1,24 @@
 class Solution {
 public:
     bool isValid(string s) {
-       stack<char> st;
+      stack<int> st;
 
-       for(char c : s){
-
-        if(c == '('|| c =='{'||c == '['){
-         st.push(c);
-        }
-        else {
+      for(char c : s){
+        
+        if(c == '('|| c == '{' || c=='['){
+            st.push(c);
+        }else{
             if(st.empty())
-            return false;
+             return false;
 
             char top = st.top();
 
-            if((c ==')' && top != '(')||(c=='}' && top != '{') || (c==']' && top != '[')){
-                return false;
-            }
-            st.pop();
-        }
-       } 
+        if(( c == ')' && top != '(')|| ( c == '}' && top != '{')|| (c == ']' && top != '['))
+        return false;
 
-       return st.empty();
+        st.pop();
+        }
+      }
+      return st.empty();
     }
 };
